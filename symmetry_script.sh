daee19fda2a3a609e26d6291b4a6a0cce7369073
WangPing521 <wp_scdedu@163.com>
2022-09-13 22:31:19 -0400

    debug

--- symmetry_script.sh
+++ symmetry_script.sh
@@ -11,19 +11,17 @@ account=def-chdesa
 save_dir=save_0913
 declare -a StringArray=(
 
-#upper_baseline
-"python -O toySeg_cons.py seed=123 lr=0.0005 weight_epoch=100 ratio=0.9 save_dir=${save_dir}/symmetry_upper305lr"
-"python -O toySeg_cons.py seed=123 lr=0.001 weight_epoch=100 ratio=0.9 save_dir=${save_dir}/symmetry_upper201lr"
-"python -O toySeg_cons.py seed=123 lr=0.005 weight_epoch=100 ratio=0.9 save_dir=${save_dir}/symmetry_upper205lr"
-"python -O toySeg_cons.py seed=123 lr=0.01  weight_epoch=100 ratio=0.9 save_dir=${save_dir}/symmetry_upper101lr"
-
-#lower_baseline
-"python -O toySeg_cons.py seed=123 lr=0.005   weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_205lr"
-"python -O toySeg_cons.py seed=123 lr=0.001   weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_201lr"
-"python -O toySeg_cons.py seed=123 lr=0.0005  weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_305lr"
-"python -O toySeg_cons.py seed=123 lr=0.0001  weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_301lr"
-"python -O toySeg_cons.py seed=123 lr=0.00005 weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_405lr"
-"python -O toySeg_cons.py seed=123 lr=0.00001 weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_401lr"
+##upper_baseline
+#"python -O toySeg_cons.py seed=123 lr=0.0005 weight_epoch=100 ratio=0.9 save_dir=${save_dir}/symmetry_upper305lr"
+#"python -O toySeg_cons.py seed=123 lr=0.001 weight_epoch=100 ratio=0.9 save_dir=${save_dir}/symmetry_upper201lr"
+#"python -O toySeg_cons.py seed=123 lr=0.005 weight_epoch=100 ratio=0.9 save_dir=${save_dir}/symmetry_upper205lr"
+#"python -O toySeg_cons.py seed=123 lr=0.01  weight_epoch=100 ratio=0.9 save_dir=${save_dir}/symmetry_upper101lr"
+#
+##lower_baseline
+#"python -O toySeg_cons.py seed=123 lr=0.0005  weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_305lr"
+#"python -O toySeg_cons.py seed=123 lr=0.0001  weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_301lr"
+#"python -O toySeg_cons.py seed=123 lr=0.00005 weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_405lr"
+#"python -O toySeg_cons.py seed=123 lr=0.00001 weight_epoch=100 ratio=0.1 save_dir=${save_dir}/symmetry_lower_401lr"
 
 # cons
 "python -O toySeg_cons.py seed=123 train_mode=cons_unlab lr=0.001 weight_epoch=15 ratio=0.1 weights.cons_weight=0.5 weights.weight_cons=0.005 save_dir=${save_dir}/symmetry_05advcons_205cons "
